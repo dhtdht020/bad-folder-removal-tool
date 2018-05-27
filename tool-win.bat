fdb87a2c479cce0d53f42d72e3fd62cd5e266c04
dhtdht020 <dhtdht020@gmail.com>
2018-05-27 15:47:53 +0300

    Able to handle errors. v1.1

--- tool-win.bat
+++ tool-win.bat
@@ -1,25 +1,49 @@
 @echo off
-echo Please make sure this batch file is located in the folder that contains the bad folder. You are currently in "%cd%"
+:start
+
 echo[
-echo -Tool by dhtdht020--------------v1-------------------------------------
+echo -Copyright (C) dhtdht020 2018---------------------------------v1.1--
 echo[
-:choice
-set /P c=Are you in the folder that contains the bad folder[Y/N]?
+echo Please make sure this batch file is located in the folder that contains the
+echo undeleteable folder.
+echo You are currently in "%cd%".
+echo[
+echo[
+
+:areyouinfolder
+
+set /P c=Are you in the folder that contains the undeleteable folder[Y/N]?
 if /I "%c%" EQU "Y" goto :Y1
 if /I "%c%" EQU "N" goto :Y2
-goto :choice
+goto :areyouinfolder
 
 
 :Y1
 
-echo Alright, please type the name of the bad folder below.
-set /p folder=Enter bad folder name: 
+echo Alright, please type the name of the undeleteable folder below.
+set /p folder=Enter undeleteable folder's name: 
 echo Alright, removing %folder%..
-rd /s "\\?\%cd%\%folder%"
+echo[
+rd /s "\\?\%cd%\%folder%" || if errorlevel 1 goto error
 pause 
-exit
 
 :Y2
 
-echo Please move me to the folder that CONTAINS the bad file! You are currently in "%cd%"
+echo Please move me to the folder that CONTAINS the undeleteable folder! You are currently in "%cd%".
+pause
+
+:error
+
+echo An error occured while removing! Information regarding the error stated above.
+echo[
+
+:retry
+
+set /P c=Retry[Y/N]?
+if /I "%c%" EQU "Y" goto :Y1
+if /I "%c%" EQU "N" goto :Y3
+goto :retry
+pause
+
+:Y3
 pause
